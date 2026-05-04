b2a20e6a903a6ed0e3adbb698c04983f24d8a12d
luizRicardoJava <luizricardo@exactus.com.br>
2026-05-03 22:10:20 -0300

    endpoint de busca e converter web para tsid string para obj

---
+++ src/main/java/com/algaworks/algasensors/device/management/api/config/web/StringToTSIDConverter.java
@@ -0,0 +1,15 @@
+package com.algaworks.algasensors.device.management.api.config.web;
+
+import io.hypersistence.tsid.TSID;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.Nullable;
+
+
+public class StringToTSIDConverter implements Converter<String, TSID> {
+
+    @Nullable
+    @Override
+    public TSID convert(String source) {
+        return TSID.from(source);
+    }
+}
--- src/main/java/com/algaworks/algasensors/device/management/api/controller/SensorController.java
+++ src/main/java/com/algaworks/algasensors/device/management/api/controller/SensorController.java
@@ -6,8 +6,10 @@ import com.algaworks.algasensors.device.management.common.IdGenerator;
 import com.algaworks.algasensors.device.management.domain.model.Sensor;
 import com.algaworks.algasensors.device.management.domain.model.SensorId;
 import com.algaworks.algasensors.device.management.domain.repository.SensorRepository;
+import io.hypersistence.tsid.TSID;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/sensors")
@@ -17,6 +19,14 @@ public class SensorController {
 
     public SensorController(SensorRepository sensorRepository) {
         this.sensorRepository = sensorRepository;
+    }
+
+    @GetMapping("/{sensorId}")
+    public SensorOutput getOne(@PathVariable TSID  sensorId) {
+        Sensor sensor = sensorRepository.findById(new SensorId(sensorId))
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        return convertToModel(sensor);
     }
 
     @PostMapping
@@ -35,13 +45,17 @@ public class SensorController {
 
         sensor = sensorRepository.saveAndFlush(sensor);
 
+        return convertToModel(sensor);
+    }
+
+    private static SensorOutput convertToModel(Sensor sensor) {
         return SensorOutput.builder()
                 .id(sensor.getId().getValue())
-                .name(input.getName())
-                .ip(input.getIp())
-                .location(input.getLocation())
-                .protocol(input.getProtocol())
-                .model(input.getModel())
+                .name(sensor.getName())
+                .ip(sensor.getIp())
+                .location(sensor.getLocation())
+                .protocol(sensor.getProtocol())
+                .model(sensor.getModel())
                 .enabled(sensor.getEnabled())
                 .build();
     }
